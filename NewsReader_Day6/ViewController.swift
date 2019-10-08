681962026918bfc50223b7efd562a1a8946fe328
YokoTanaka0711 <prymjou@gmail.com>
2019-10-08 12:13:02 +0900

    Initial Commit

---
+++ NewsReader_Day6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsReader_Day6
+//
+//  Created by 田中陽子 on 2019/10/08.
+//  Copyright © 2019 Yoko Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
